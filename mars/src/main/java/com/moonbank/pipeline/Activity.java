54c6d755ed9032271559b3673dd93fdf091e7713
Kam Lok Chan <kamlokchan@Kams-Mac-mini.local>
2022-10-24 01:15:33 +0800

    add no argument constuctor

--- mars/src/main/java/com/moonbank/pipeline/Activity.java
+++ mars/src/main/java/com/moonbank/pipeline/Activity.java
@@ -2,12 +2,14 @@ package com.moonbank.pipeline;
 
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 
 import java.math.BigDecimal;
 
 @Builder
+@NoArgsConstructor
 @DefaultSchema(JavaFieldSchema.class)
 @EqualsAndHashCode
 public class Activity {
