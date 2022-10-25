d8b7161e05a532094a8ba470645606c9ac7372cd
Kam Lok Chan <kamlokchan@Kams-Mac-mini.local>
2022-10-25 08:20:15 +0800

    update mars pipeline

--- mars/src/main/java/com/moonbank/pipeline/MarsActivity.java
+++ mars/src/main/java/com/moonbank/pipeline/MarsActivity.java
@@ -1,15 +1,14 @@
 package com.moonbank.pipeline;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+import org.apache.beam.sdk.schemas.annotations.SchemaCreate;
 
 import java.math.BigDecimal;
 
 @Builder
-@AllArgsConstructor
 @DefaultSchema(JavaFieldSchema.class)
 @EqualsAndHashCode
 public class MarsActivity {
@@ -21,6 +20,23 @@ public class MarsActivity {
     String destAccount;
     BigDecimal amount;
     String customerName;
+
+    @SchemaCreate
+    public MarsActivity(String timestamp,
+                        String ipAddr,
+                        String action,
+                        String srcAccount,
+                        String destAccount,
+                        BigDecimal amount,
+                        String customerName) {
+        this.timestamp = timestamp;
+        this.ipAddr = ipAddr;
+        this.action = action;
+        this.srcAccount = srcAccount;
+        this.destAccount = destAccount;
+        this.amount = amount;
+        this.customerName = customerName;
+    }
 
     public static MarsActivity fromCsv(String input) {
         var output = input.split(",");
